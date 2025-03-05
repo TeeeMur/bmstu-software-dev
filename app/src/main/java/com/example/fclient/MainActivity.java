8da1eed54270aa24a993017da81c5a1cd3c3a8e8
teemur <timurs.kzn@gmail.com>
2025-03-05 08:54:30 +0300

    initial commit

---
+++ app/src/main/java/com/example/fclient/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.fclient.databinding.ActivityMainBinding;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int randomRes = initRng();
+        byte[] byteArr = randomBytes(20);
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+        byte[] KEY = randomBytes(16);
+        Log.d("fclient_ndk", Arrays.toString(byteArr));
+        byteArr = encrypt(KEY, byteArr);
+        Log.d("fclient_ndk", Arrays.toString(byteArr));
+        byteArr = decrypt(KEY, byteArr);
+        Log.d("fclient_ndk", Arrays.toString(byteArr));
+        byte[] encryptedTextBytes = encrypt(KEY, tv.getText().toString().getBytes(StandardCharsets.UTF_8));
+        String decryptedText = new String(decrypt(KEY, encryptedTextBytes), StandardCharsets.UTF_8);
+        binding.sampleText2.setText("Шифрованный текст:" + Arrays.toString(encryptedTextBytes));
+        binding.sampleText3.setText("Расшифрованный текст:" + decryptedText);
+        Log.d("fclient_ndk", Arrays.toString(tv.getText().toString().getBytes(StandardCharsets.UTF_8)));
+        Log.d("fclient_ndk", Arrays.toString(decrypt(KEY, encryptedTextBytes)));
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
