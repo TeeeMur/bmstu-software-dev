f20fd43192f67c12f19d8cc7ab599598a35cb98d
teemur <timurs.kzn@gmail.com>
2025-05-02 15:29:38 +0300

    9th lab commit

--- front/src/App.js
+++ front/src/App.js
@@ -2,9 +2,9 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
+import Login from "./components/Login"
 
 function App() {
     return (
@@ -14,6 +14,7 @@ function App() {
                 <div className="container-fluid">
                     <Routes>
                         <Route path="home" element={<Home />}/>
+                        <Route path="login" element={<Login />}/>
                     </Routes>
                 </div>
             </BrowserRouter>
---
+++ front/src/utils/Utils.jsx
@@ -0,0 +1,32 @@
+class Utils {
+
+    saveUser(user) {
+        localStorage.setItem('user', JSON.stringify(user))
+    }
+
+    removeUser() {
+        localStorage.removeItem('user')
+    }
+
+    getToken()
+    {
+        let user = JSON.parse(localStorage.getItem('user'))
+        console.log(user)
+        console.log(user && "Bearer " + user.token)
+        return user && "Bearer " + user.token;
+    }
+
+    getUserName()
+    {
+        let user = JSON.parse(localStorage.getItem('user'))
+        return user && user.login;
+    }
+
+    getUser()
+    {
+        return JSON.parse(localStorage.getItem('user'))
+    }
+}
+
+export default new Utils()
+            
